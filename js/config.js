648a559fd399b323c7b7ed77067fef92d4eee5f1
Atharva Chitre <100697714+whoatharva@users.noreply.github.com>
2025-06-05 14:50:11 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_Otu9ITxGz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7smg5qg7nvv0rc0ga9ghenf9js', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
